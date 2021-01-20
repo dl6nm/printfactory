3417694aedf8f0b78aa7c5c19b63e479421a8513
dl6nm <mail@dl6nm.de>
2021-01-20 11:45:48 +0100

    test: add tests for Printer class

--- tests/unit/test_printer.py
+++ tests/unit/test_printer.py
@@ -1,34 +1,52 @@
 import pytest
 
-from printfactory.printer import Printer
+from printfactory import Printer
 
 
 @pytest.mark.parametrize(
-    argnames='print_file_name',
-    argvalues=['my.pdf'],
+    argnames=['printer_name', 'printer_driver', 'printer_port'],
+    argvalues=[
+        [None, None, None],
+        ['MyPrinter', None, None],
+        ['MyPrinter', 'MyDriver', None],
+        ['MyPrinter', 'MyDriver', 'Portname_1234'],
+    ],
 )
-class TestPrinterDefaults:
-    """Test default Printer"""
+class TestPrinter:
+    """Test Printer class"""
 
-    printer = Printer()
+    def test_attributes(self, printer, printer_name, printer_driver, printer_port):
+        assert printer.name == printer_name
+        assert printer.driver == printer_driver
+        assert printer.port == printer_port
 
-    def test_default_attributes(self):
-        assert self.printer.name == 'BlackHole'
-        assert self.printer.driver is None
-        assert self.printer.port is None
-
-    def test_get_list(self):
-        # instance method
-        assert 'BlackHole' in Printer.get_list()
+    def test_get_list(self, printer, printer_name, printer_driver, printer_port):
         # static method
-        assert 'BlackHole' in self.printer.get_list()
+        printers = Printer.get_list()
+        assert len(printer) >= 0
+        assert 'BlackHole' in printers
 
-    def test_get_default(self):
-        # instance method
-        assert 'BlackHole' in self.printer.get_default()
-        # static method
-        assert 'BlackHole' in Printer.get_default()
+    def test_get_default(self, printer):
+        default_printer = Printer.get_default()
+        assert len(default_printer) == 1
+        assert isinstance(default_printer, Printer)
 
-    def test_send_file(self, shared_datadir, print_file_name):
-        print_file = shared_datadir / print_file_name
-        assert self.printer.send_file(print_file=print_file) is None
+
+class TestPrinterFail:
+    """Test Printer class - Failing tests"""
+
+    @pytest.mark.parametrize(
+        argnames=['printer_name', 'printer_driver', 'printer_port', 'exception_msg'],
+        argvalues=[
+            [None, 'MyDriverName', None, 'Missing printer'],
+            ['MyPrinter', None, 1234, 'Missing driver'],
+            [None, 'MyDriverName', 'MyPrinterPort', 'Missing printer'],
+            [None, None, 'MyPrinterPort', 'Missing printer'],
+        ],
+        ids=None,
+    )
+    def test_fail_missing_attributes(self, printer, printer_name, printer_driver, printer_port, exception_msg):
+        with pytest.raises(TypeError) as execinfo:
+            printer
+        assert execinfo.value.args[0] == exception_msg
+

b94794b4465ad6d1499ac3b79a138a078500a46c
dl6nm <mail@dl6nm.de>
2021-01-21 10:03:35 +0100

    test: add attribute "default" to Printer class
    This is for checking if a printer is the defaults system printer.

--- tests/unit/test_printer.py
+++ tests/unit/test_printer.py
@@ -7,24 +7,29 @@ class TestPrinter:
     """Test Printer class"""
 
     @pytest.mark.parametrize(
-        argnames=['printer_name', 'driver_name', 'port_name'],
+        argnames=['printer_name', 'driver_name', 'port_name', 'default'],
         argvalues=[
-            [None, None, None],
-            ['MyPrinter', None, None],
-            ['MyPrinter', 'MyDriver', None],
-            ['MyPrinter', 'MyDriver', 'Portname_1234'],
+            [None, None, None, True],
+            ['MyPrinter', None, None, True],
+            ['MyPrinter', 'MyDriver', None, True],
+            ['MyPrinter', 'MyDriver', 'Portname_1234', True],
         ],
     )
-    def test_attributes(self, printer, printer_name, driver_name, port_name):
+    def test_attributes(self, printer, printer_name, driver_name, port_name, default):
         assert printer.name == printer_name
         assert printer.driver == driver_name
         assert printer.port == port_name
+        assert printer.default == default
 
     def test_get_list(self):
         # static method
         printers = Printer.get_list()
+        assert isinstance(printers, list)
         assert len(printers) > 0
-        assert 'BlackHole' in printers
+        for printer in printers:
+            assert isinstance(printer, Printer)
+
+        # assert 'BlackHole' in printers
 
     def test_get_default(self):
         default_printer = Printer.get_default()
@@ -36,16 +41,16 @@ class TestPrinterFail:
     """Test Printer class - Failing tests"""
 
     @pytest.mark.parametrize(
-        argnames=['printer_name', 'driver_name', 'port_name', 'exception_msg'],
+        argnames=['printer_name', 'driver_name', 'port_name', 'default', 'exception_msg'],
         argvalues=[
-            [None, 'MyDriverName', None, 'Missing printer'],
-            ['MyPrinter', None, 1234, 'Missing driver'],
-            [None, 'MyDriverName', 'MyPrinterPort', 'Missing printer'],
-            [None, None, 'MyPrinterPort', 'Missing printer'],
+            [None, 'MyDriverName', None, True, 'Missing printer'],
+            ['MyPrinter', None, 1234, True, 'Missing driver'],
+            [None, 'MyDriverName', 'MyPrinterPort', True, 'Missing printer'],
+            [None, None, 'MyPrinterPort', True, 'Missing printer'],
         ],
         ids=None,
     )
-    def test_fail_missing_attributes(self, printer, printer_name, driver_name, port_name, exception_msg):
+    def test_fail_missing_attributes(self, printer, printer_name, driver_name, port_name, default, exception_msg):
         with pytest.raises(TypeError) as execinfo:
             printer
         assert execinfo.value.args[0] == exception_msg

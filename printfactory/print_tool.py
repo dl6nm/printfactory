8c6abc4ddaba51098988359224bd631e33a6491b
dl6nm <mail@dl6nm.de>
2021-01-27 12:44:50 +0100

    feat: implement PrintTool methods exists(), get_args() and set_args()

--- printfactory/print_tool.py
+++ printfactory/print_tool.py
@@ -1,5 +1,7 @@
 import pathlib
 
+from typing import List
+
 from . import Printer
 
 
@@ -7,7 +9,18 @@ class PrintTool:
     """Generic PrintTool"""
 
     def __init__(self, name, printer, app_path, args):
-        self.name = name
-        self.printer = printer
-        self.app_path = app_path
+        self.name: str = name
+        self.printer: Printer = printer
+        self.app_path: pathlib.Path = app_path
+        self.args: List[str] = args
+
+    def exists(self):
+        app_path_exists = self.app_path.exists()
+        return app_path_exists
+
+    def get_args(self):
+        return self.args
+
+    def set_args(self, args):
         self.args = args
+        return self.args

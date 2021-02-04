48f93d6443c60e6eb27a756969c6334e7fbe5d7b
dl6nm <mail@dl6nm.de>
2021-02-04 12:28:56 +0100

    test: add test for failing initialization of AdobeReader class

--- tests/unit/print_tools/test_adobe_reader.py
+++ tests/unit/print_tools/test_adobe_reader.py
@@ -18,6 +18,7 @@ from printfactory import AdobeReader, Printer, PrintTool
     ],
 )
 class TestAdobeReader:
+    """Adobe Reader class tests"""
 
     def test_initialization(self, name, printer, app_path, args, exists):
         reader = AdobeReader(
@@ -29,3 +30,26 @@ class TestAdobeReader:
         assert reader.app_path == app_path
         assert reader.get_args() == args
         assert reader.exists() is exists
+
+
+@pytest.mark.parametrize(
+    argnames=['name', 'printer', 'app_path', 'args', 'exists'],
+    argvalues=[
+        [
+            'Adobe Reader',
+            Printer('MyPrinterName', 'MyDriverName', 'MyPortName'),
+            pathlib.Path(),
+            ['/t', 'MyPrintFile.pdf', 'MyPrinterName', 'MyDriverName', 'MyPortName'],
+            True,
+        ],
+    ],
+)
+class TestAdobeReaderFail:
+    """Adobe Reader class - Failing tests"""
+
+    def test_initialization(self, name, printer, app_path, args, exists):
+        with pytest.raises(FileNotFoundError) as execinfo:
+            AdobeReader(
+                printer=printer,
+            )
+        assert execinfo.value.args[0] == 'PrintTool "." does not exist'

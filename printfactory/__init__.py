c4d57caeb0db2214de30745d495cfab1d179f151
dl6nm <mail@dl6nm.de>
2021-02-08 14:35:25 +0100

    fix: change printfactory base import for adobe print_tools

--- printfactory/__init__.py
+++ printfactory/__init__.py
@@ -5,10 +5,11 @@ __version__ = version('printfactory')
 
 from .printer import *
 from .print_tool import *
-from .print_tools.adobe_reader import *
+from .print_tools.adobe import *
 
 __all__ = [
     'Printer',
     'PrintTool',
     'AdobeReader',
+    'AdobeAcrobat',
 ]
